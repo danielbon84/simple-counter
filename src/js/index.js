9701f75e3956084da310c4c5c08d88f9b8fefad9
danielbon84 <daniel_bon84@hotmail.com>
2023-01-19 22:19:19 +0000

    contador

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,50 @@
-//import react into the bundle
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
+
 import "../styles/index.css";
+import SecondCounter from "./component/secondCounter.jsx";
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let one = 0;
+let two =0;
+let three = 0;
+let four = 0;
+let five = 0;
+let six =0;
+
+setInterval(() => {
+    one++;
+    if(one > 9){
+      one = 0;
+      two++;
+    }
+
+    if(two > 9){
+        two = 0;
+        three++;
+      }
+
+      if(three > 9){
+        three = 0;
+        four++;
+      }
+      if(four > 9){
+        four = 0;
+        five++;
+      }
+      if(five > 9){
+        five = 0;
+        six++;
+      }
+      
+
+     console.log(six,five,four, three, two, one);
+    
+     ReactDOM.render(
+        <SecondCounter one={one} two={two} three={three} four={four} five={five} six={six}/>,
+        document.querySelector("#app")
+      );
+    }, 1000);
